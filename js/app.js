17a8cc1d41b60cca8e2ce36c8f9a82b9a58b0917
renamos <rene_ramos@me.com>
2016-09-02 00:05:29 -0700

    V1 of jQuery Street Fighter Exercise
    This is the first working version of this exercise. Demonstrates my
    understanding of jQuery, as well as HTML/CSS.

---
+++ js/app.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+    $('.ryu').mouseenter(function () {
+        $('.ryu-still').hide();
+        $('.ryu-ready').show();
+    }).mouseleave(function () {
+        $('.ryu-ready').hide();
+        $('.ryu-still').show();
+    }).mousedown(function () {
+        playHadouken();
+        $('.ryu-ready').hide();
+        $('.ryu-throwing').show();
+        $('.hadouken').finish().show().animate({
+            'left': '1020px'
+        }, 500, function () {
+            $('.hadouken').hide();
+            $('.hadouken').css('left', '600px');
+        });
+    })
+        .mouseup(function () {
+        $('.ryu-throwing').hide();
+        $('.ryu-ready').show();
+    });
+    $('body').keydown(function(e){
+                     if(e.which == 88) {
+                $('.ryu-still').hide();
+                $('.ryu-cool').show();
+                         
+                         }
+    })
+    
+    .keyup(function (e) {
+        $('.ryu-still').show();
+                $('.ryu-cool').hide();
+           });
+                                    
+                      });
+
+
+function playHadouken() {
+    $('#hadouken-sound')[0].volume = 0.5;
+    $('#hadouken-sound')[0].load();
+    $('#hadouken-sound')[0].play();
+}
